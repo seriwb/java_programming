99cc1564fa77d80ad1e9316fc0ec6a987ef8cf9a
seri <seri.wb@gmail.com>
2015-01-04 14:00:49 +0900

    change input/output parameter type

--- src/main/java/white/box/reflection/rap/ExecuteUtil.java
+++ src/main/java/white/box/reflection/rap/ExecuteUtil.java
@@ -4,42 +4,41 @@ import java.lang.reflect.Method;
 
 /**
  * 特定のメソッドの実行を助けるユーティリティクラス
+ * 
  * @author seri
  */
 public class ExecuteUtil {
 
-	private static final String SPLIST_KEY_INPUT = "Input";
+	private static final String SPLIST_KEY = "Input";
 	private static final String ADD_OUTPUT_CLASS_NAME = "Output";
-	
-	public static Object search(Object input) {
-	
+
+	public static DefaultOutput search(DefaultInput input) {
+
 		CommonDao dao = new CommonDao();
-		
+
 		// 共通の文字列を取得（inputの先頭文字を取得して利用する）
-		String tablename = input.getClass().getSimpleName().split(SPLIST_KEY_INPUT)[0];
-		
+		String tablename = input.getClass().getSimpleName().split(SPLIST_KEY)[0];
+
 		try {
-	        @SuppressWarnings("unchecked")
-            Class<? extends DefaultOutput> outputclazz =
-	        		(Class <? extends DefaultOutput>) Class.forName(
-	        				input.getClass().getName().split(SPLIST_KEY_INPUT)[0].concat(ADD_OUTPUT_CLASS_NAME));
-	        System.out.println(outputclazz.getName());
-	        
-			return dao.dbExecute(tablename.concat(".search_pk"), (DefaultInput) input, outputclazz);
-		
-        } catch (ClassNotFoundException e) {
-	        e.printStackTrace();
-        }
-		
+			@SuppressWarnings("unchecked")
+			Class<? extends DefaultOutput> outputclazz = (Class<? extends DefaultOutput>) Class.forName(
+					input.getClass().getName().split(SPLIST_KEY)[0].concat(ADD_OUTPUT_CLASS_NAME));
+			System.out.println(outputclazz.getName());
+
+			return dao.dbExecute(tablename.concat(".search_pk"), input, outputclazz);
+
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+
 		return null;
 	}
-	
+
 }
 
 class CommonDao {
-	public DefaultOutput dbExecute(
-			String sqlId, DefaultInput input, Class<? extends DefaultOutput> outputclazz) {
-		
+	public DefaultOutput dbExecute(String sqlId, DefaultInput input, Class<? extends DefaultOutput> outputclazz) {
+
 		// ダミーのメイン処理のようなもの
 		System.out.println(input.getClass().getName());
 		System.out.println("SQL ID:" + sqlId);
@@ -49,21 +48,21 @@ class CommonDao {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
+
 		// 返却用クラスに値を設定
 		DefaultOutput output = null;
-        try {
-	        output = outputclazz.newInstance();
-        } catch (InstantiationException | IllegalAccessException e1) {
-	        e1.printStackTrace();
-        }
+		try {
+			output = outputclazz.newInstance();
+		} catch (InstantiationException | IllegalAccessException e1) {
+			e1.printStackTrace();
+		}
 		try {
 			Method m = outputclazz.getMethod("setResult", String.class);
 			m.invoke(output, "OK");
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
+
 		return output;
 	}
 
